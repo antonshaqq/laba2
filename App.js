c4261dbbb5e867c2f401c6bfa6c205e4d15c3923
antonshaqq <86612952+antonshaqq@users.noreply.github.com>
2021-06-28 07:40:42 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { NavBar } from './src/components/NavBar';
+import { MainScreen } from './src/screens/MainScreen';
+import { TodoScreen } from './src/screens/TodoScreen';
+
+export default function App() {
+  const [todoId, setTodoId] = useState(null);
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'Когда же это кончится' },
+    { id: '2', title: 'Быстрей бы весна' },
+    { id: '3', title: 'Антон Сычев усталл...' },
+  ]);
+
+  const addTodo = (title) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
+  };
+
+  const removeTodo = (id) => {
+    const todo = todos.find((t) => t.id === id);
+    Alert.alert(
+      'Удаление элемента',
+      `Вы уверены, что хотите удалить ${todo.title}`,
+      [
+        {
+          text: 'Отмена',
+          style: 'cancel',
+        },
+        {
+          text: 'Удалить',
+          onPress: () => {
+            setTodoId(null);
+            setTodos((prev) => prev.filter((todo) => todo.id !== id));
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const updateTodo = (id, title) => {
+    setTodos((old) =>
+      old.map((todo) => {
+        if (todo.id === id) {
+          todo.title = title;
+        }
+        return todo;
+      })
+    );
+  };
+
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodoId}
+    />
+  );
+
+  if (todoId) {
+    const selectedTodo = todos.find((todo) => todo.id === todoId);
+    content = (
+      <TodoScreen
+        goBack={() => {
+          setTodoId(null);
+        }}
+        todo={selectedTodo}
+        onRemove={removeTodo}
+        onSave={updateTodo}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <NavBar title="Todo App" />
+      <View style={StyleSheet.container}>{content}</View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+});
